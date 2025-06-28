8f99fb093493bd5565f7966e52bed855f5a82988
MuskanChablani29 <chablanimuskan@gmail.com>
2025-06-28 10:10:17 +0530

    fixed Authentication issue

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from . import views
 
+def home(request):
+    return JsonResponse({'message': 'Miracle IT Career Academy API is running'})
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/courses/', include('courses.urls')),
